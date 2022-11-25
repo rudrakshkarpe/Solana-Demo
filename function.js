c3f98f4cb53d8a3817037205e45cc85432661afe
rudrakshkarpe <rudraksh.karpe@gmail.com>
2022-11-25 16:18:53 +0530

    Add files

--- function.js
+++ function.js
@@ -10,5 +10,9 @@ const square = (x) => {
     return x * x;
 }
 
-
 // Path: Solana-Demo\main.js
+
+// use NPM to install the package
+// checkout -> https://www.npmjs.com/
+
+// keep googling for the package you want to use or anything!
---
+++ package.js
@@ -0,0 +1,25 @@
+var cowsay = require("cowsay");
+
+console.log(
+    cowsay.say({
+        text: "I'm a moooodule",
+        e: "oO",
+        T: "U ",
+    })
+);
+
+
+// json -> javascript object notation
+// json -> key value pair
+
+// const person_details = {
+//     name: "Rudraksh Karpe",
+//     age: 20,
+//     hobbies: ["coding", "reading", "writing"],
+//     isAlive: true,
+//     address: {w
+//         city: "Pune",
+//         state: "Maharashtra",
+//         country: "India",
+//     },
+// };
