f982b3af45ccde1babfd2222311684ef08c2208a
rudrakshkarpe <rudraksh.karpe@gmail.com>
2022-11-25 15:58:01 +0530

    update main.js

---
+++ main.js
@@ -0,0 +1,39 @@
+// printing statements
+console.log("This is rudrakshkarpe!");
+
+const a = 10;
+const b = 20;
+
+console.log(a + b);
+
+// if else condition 
+
+if (a > b) {
+    console.log("a is greater than b");
+} else {
+    console.log("b is greater than a");
+}
+
+// always create a new variable and use const
+
+// loops
+//initialisation
+//condition
+//increment / decrement
+
+for (let i = 0; i < n;i++) {
+    console.log(i);
+}
+
+
+// try catch block! 
+
+if (a > b) {
+    console.log("Cod ei working");
+} else {
+    try {
+        throw new Error("A i slesser than B");
+    }catch (err) {
+        console.log(err);
+    }
+}
